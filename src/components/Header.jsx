45c35b56dc986d7e495633211f3da12439d65caa
Rafael Venancio <rafael.venancio08.06@gmail.com>
2025-02-12 14:02:43 -0300

    FullStack: Components refactor and Routes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,16 @@
 import React from "react";
-import logoSpotify from "../assets/logo/spotify-logo.png"
+import logoSpotify from "../assets/logo/spotify-logo.png";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return <div className="header">
-    <img src={logoSpotify} alt="Logo do spotify" />
+    <Link to="/">
+      <img src={logoSpotify} alt="Logo do spotify" />
+    </Link>
     
-    <a className="header__link" href="/">
+    <Link className="header__link" to="/">
       <h1>Spotify</h1>
-    </a>
+    </Link>
     
   </div>
 };
