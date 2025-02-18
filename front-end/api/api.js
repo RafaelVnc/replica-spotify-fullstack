6fc0de77721d13871ea8ba61d036ce008a65555b
Rafael Venancio <rafael.venancio08.06@gmail.com>
2025-02-18 20:45:08 -0300

    Ready to deploy

--- front-end/api/api.js
+++ front-end/api/api.js
@@ -1,6 +1,10 @@
+import dotenv from "dotenv";
 import axios from "axios";
 
-const URL = "http://localhost:3000";
+dotenv.config();
+const NODE_ENV = process.env.NODE_ENV;
+
+const URL = NODE_ENV === 'development' ? "http://localhost:3000/api" : "/api";
 
 const responseArtists = await axios.get(`${URL}/artists`);
 const responseSongs = await axios.get(`${URL}/songs`);
